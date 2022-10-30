d33af7436463c50f2f96cbafb9be5410cb549109
rabobahago <rabobahago@gmail.com>
2022-10-30 20:57:58 +0100

    NFT marketplce solidity token created

---
+++ contracts/NFTMarket.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+contracts NFT is ERC721URIStorage{
+    // importing counter from openzeppelin
+  using Counters for Counters.Counter;
+  //set _tokenIds to be of type Counters.Counter
+  Counters.Counter private _tokenIds;
+  // address of the NFT Marketplace
+  address contractAddress;
+  constructor(address marketplace) ERC721('rabosea token', 'PNVT'){
+    contractAddress = marketplace;
+  }
+  function createToken(string memory tokenURL) public returns(uint256){
+     _tokenIds.increment();
+     uint256 newItemId = _tokenIds.current();
+     _mint(msg.sender, newItemId);
+     _setTokenURL(newItemId, tokenURL);
+     setApprovalForAll(contractAddress, true);
+     return newItemId;
+  }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,25 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-//   const accounts = await hre.ethers.getSigners();
-
-//   for (const account of accounts) {
-//     console.log(account.address);
-//   }
-// });
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-//yarn add --dev "hardhat@^2.7.1" "@nomiclabs/hardhat-waffle@^2.0.0" "ethereum-waffle@^3.0.0" "chai@^4.2.0" "@nomiclabs/hardhat-ethers@^2.0.0" "ethers@^5.0.0"
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-//the version of solidity matter
-
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+const { PRIVATE_KEY, PROJECT_PRIVATE_KEY } = process.env;
+const API_URL = "https://eth-goerli.g.alchemy.com/v2";
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    goerli: {
+      url: `${API_URL}/${PROJECT_PRIVATE_KEY}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
