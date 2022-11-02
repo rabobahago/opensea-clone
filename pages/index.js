440e930445c1e5065e694a69aa7250f6ef8dc67d
rabobahago <rabobahago@gmail.com>
2022-11-02 14:27:11 +0100

    create item input and file upload added

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,22 @@
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import styles from "../styles/Home.module.css";
 import Market from "../artifacts/contracts/NFTMarket.sol/NFTMarket.json";
 import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
 const { nftaddress, nftmarketaddress } = require("../config");
-const API_KEY = process.env.API_KEY;
+const { PROJECT_PRIVATE_KEY } = process.env;
 let network = "goerli";
 export default function Home() {
   const [nfts, setNfts] = useState([]);
   const [loadingState, setLoadingState] = useState("");
   useEffect(() => {
-    loadNFts();
+    loadNFTs();
   }, []);
   async function loadNFTs() {
-    const provider = new ethers.providers.AlchemyProvider(network, API_KEY);
+    const provider = new ethers.providers.AlchemyProvider(
+      network,
+      PROJECT_PRIVATE_KEY
+    );
 
     const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider);
     const marketContract = new ethers.Contract(
@@ -50,12 +52,53 @@ export default function Home() {
     const signer = provider.getSigner();
     const contract = new ethers.Contract(nftmarketaddress, Market.abi, signer);
     const price = ethers.utils.parseUnits(nfts.price.toString(), "ether");
+    const transaction = await contract.createMarketSale(
+      nftaddress,
+      nft.tokenId,
+      {
+        value: price,
+      }
+    );
+    await transaction.wait();
+    loadNFTs();
   }
   if (loadingState === "loaded" && !nfts.length)
     return <h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>;
   return (
-    <div className={styles.container}>
-      <h1>Globals</h1>
+    <div className="flex justify-center">
+      <div className="px-4" style={{ maxWidth: "1600px" }}>
+        <div className="grid grid-col-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+          {nfts.map((nft, i) => (
+            <div className="border shadow rounded-xl overflow-hidden">
+              <img src={nft.image} alt="nft" />
+              <div className="p-4">
+                <p
+                  style={{ height: "64px" }}
+                  className="text-2xl font-semibold"
+                >
+                  {nft.name}
+                </p>
+                <div style={{ height: "70px", overflow: "hidden" }}>
+                  <p className="text-gray-400">{nft.description}</p>
+                </div>
+              </div>
+              <div className="p-4 bg-black">
+                <p className="text-2xl font-bold text-white">
+                  {nft.price} MATIC
+                </p>
+                <button
+                  className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded"
+                  onClick={() => buyNFT(nft)}
+                >
+                  Buy
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
+// nftMarket deployed to: 0x5FbDB2315678afecb367f032d93F642f64180aa3
+// nft deployed to: 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
